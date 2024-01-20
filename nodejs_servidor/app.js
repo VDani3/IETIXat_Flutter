25118417c6c9c0289586ad8c0465ca574ca53d38
jmartintoro <jmartintoro.cf@iesesteveterradas.cat>
2024-01-20 20:47:42 +0100

    server

--- nodejs_servidor/app.js
+++ nodejs_servidor/app.js
@@ -5,84 +5,30 @@ const url = require('url')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Configurar la rebuda d'arxius a través de POST
-const storage = multer.memoryStorage(); // Guardarà l'arxiu a la memòria
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Tots els arxius de la carpeta 'public' estàn disponibles a través del servidor
-// http://localhost:3000/
-// http://localhost:3000/images/imgO.png
 app.use(express.static('public'))
-
-// Configurar per rebre dades POST en format JSON
 app.use(express.json());
 
-// Activar el servidor HTTP
-const httpServer = app.listen(port, appListen)
-async function appListen() {
+const httpServer = app.listen(port, async () => {
   console.log(`Listening for HTTP queries on: http://localhost:${port}`)
-}
+})
 
-// Tancar adequadament les connexions quan el servidor es tanqui
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
+
 function shutDown() {
   console.log('Received kill signal, shutting down gracefully');
   httpServer.close()
   process.exit(0);
 }
 
-// Configurar direcció tipus 'GET' amb la URL ‘/itei per retornar codi HTML
-// http://localhost:3000/ieti
-app.get('/ieti', getIeti)
-async function getIeti(req, res) {
-
-  // Aquí s'executen totes les accions necessaries
-  // - fer una petició a un altre servidor
-  // - consultar la base de dades
-  // - calcular un resultat
-  // - cridar la linia de comandes
-  // - etc.
-
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.end('<html><head><meta charset="UTF-8"></head><body><b>El millor</b> institut del món!</body></html>')
-}
-
-// Configurar direcció tipus 'GET' amb la URL ‘/llistat’ i paràmetres URL 
-// http://localhost:3000/llistat?cerca=cotxes&color=blau
-// http://localhost:3000/llistat?cerca=motos&color=vermell
-app.get('/llistat', getLlistat)
-async function getLlistat(req, res) {
-  let query = url.parse(req.url, true).query;
-
-  // Aquí s'executen totes les accions necessaries
-  // però tenint en compte els valors dels variables de la URL
-  // que guardem a l'objecte 'query'
-
-  if (query.cerca && query.color) {
-    // Així es retorna un text per parts (chunks)
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' });
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    res.write(`result: "Aquí tens el llistat de ${query.cerca} de color ${query.color}"`)
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    res.write(`\n list: ["item0", "item1", "item2"]`)
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    res.end(`\n end: "Això és tot"`)
-  } else {
-    // Així es retorna un objecte JSON directament
-    res.status(400).json({ result: "Paràmetres incorrectes" })
-  }
-}
-
-// Configurar direcció tipus 'POST' amb la URL ‘/data'
-// Enlloc de fer una crida des d'un navegador, fer servir 'curl'
-// curl -X POST -F "data={\"type\":\"test\"}" -F "file=@package.json" http://localhost:3000/data
 app.post('/data', upload.single('file'), async (req, res) => {
-  // Processar les dades del formulari i l'arxiu adjunt
   const textPost = req.body;
   const uploadedFile = req.file;
   let objPost = {}
-
+  console.log(textPost.data);
   try {
     objPost = JSON.parse(textPost.data)
   } catch (error) {
@@ -91,27 +37,124 @@ app.post('/data', upload.single('file'), async (req, res) => {
     return
   }
 
-  // Aquí s'executen totes les accions necessaries
-  // però tenint en compte el tipus de petició 
-  // (en aquest exemple només 'test')
 
-  // A l'exercici 'XatIETI' hi hauràn dos tipus de petició:
-  // - 'conversa' que retornara una petició generada per 'mistral'
-  // - 'imatge' que retornara la interpretació d'una imatge enviada a 'llava'
+  if (objPost.type === 'conversa') {
+    console.log('message received "conversa"')
+    try {
+      const messageText = objPost.text;
+      let url = 'http://localhost:11434/api/generate';
+      var data = {
+        model: "mistral",
+        prompt: messageText
+      };
 
-  if (objPost.type === 'test') {
-    if (uploadedFile) {
-      let fileContent = uploadedFile.buffer.toString('utf-8')
-      console.log('Contingut de l\'arxiu adjunt:')
-      console.log(fileContent)
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" // Configurar el tipo de contenido según tus necesidades
+        },
+        body: JSON.stringify(data) // Convertir los datos a formato JSON si es necesario
+      }).then(function (respuesta) {
+        // Verificar si la respuesta es exitosa
+        if (!respuesta.ok) {
+          throw new Error("AAAAAAAAAAAAAAAAAAAAAAError en la solicitud");
+        }
+        // Leer y procesar la respuesta
+        return respuesta.text();
+      })
+      .then(function (datosRespuesta) {
+        // Manipular los datos de la respuesta
+        var lineas = datosRespuesta.split('\n');
+
+        var objetosJSON = [];
+        for (var i = 0; i < lineas.length; i++) {
+          var linea = lineas[i].trim(); 
+          if (linea) {
+            objetosJSON.push(JSON.parse(linea));
+          }
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
+        var resp = "";
+        objetosJSON.forEach(function(objeto) {
+          resp = resp + objeto.response;
+          res.write(objeto.response);
+        });
+        
+        
+        res.end("")
+      })
+      .catch(function (error) {
+        console.error("EEError en la solicitud:", error);
+      });
+      
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error processing request.');
     }
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
-    res.write("POST First line\n")
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    res.write("POST Second line\n")
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    res.end("POST Last line\n")
+
+  } else if (objPost.type === 'image') {
+    console.log('message received "imatge"')
+    try {
+      const messageText = objPost.text;
+      const image = [objPost.image];
+      let url = 'http://localhost:11434/api/generate';
+      var data = {
+        model: "llava",
+        prompt: messageText,
+        images: image
+      };
+      
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" // Configurar el tipo de contenido según tus necesidades
+        },
+        body: JSON.stringify(data) // Convertir los datos a formato JSON si es necesario
+      }).then(function (respuesta) {
+        // Verificar si la respuesta es exitosa
+        if (!respuesta.ok) {
+          res.status(400).send('Error en la solicitud.')
+          throw new Error("Error en la solicitud");
+        }
+        // Leer y procesar la respuesta
+        return respuesta.text();
+      })
+      .then(function (datosRespuesta) {
+        // Manipular los datos de la respuesta
+        var lineas = datosRespuesta.split('\n');
+
+        var objetosJSON = [];
+        for (var i = 0; i < lineas.length; i++) {
+          var linea = lineas[i].trim(); 
+          if (linea) {
+            objetosJSON.push(JSON.parse(linea));
+          }
+        }
+
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' })
+        var resp = "";
+        objetosJSON.forEach(function(objeto) {
+          resp = resp + objeto.response;
+          res.write(objeto.response);
+        });
+        
+        
+        res.end("")
+      })
+      .catch(function (error) {
+        console.error("EEError en la solicitud:", error);
+      });
+      
+      
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Error processing request.');
+    }
   } else {
+    console.log('error, type not exists')
     res.status(400).send('Sol·licitud incorrecta.')
   }
 })
+
